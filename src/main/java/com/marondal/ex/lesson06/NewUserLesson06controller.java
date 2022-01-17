7394cfaee7185fdc11ef8733dc61c959ccc75598
DESKTOP-THLL0VQ\eje93 <jeongyi0131@naver.com>
2022-01-17 16:21:01 +0900

    [Home]

---
+++ src/main/java/com/marondal/ex/lesson06/NewUserLesson06controller.java
@@ -0,0 +1,12 @@
+package com.marondal.ex.lesson06;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NewUserLesson06controller {
+
+	
+	public String ex01() {
+		return "lesson04/addUser";
+	}
+}
